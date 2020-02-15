a1838df09d6cd0af5e6ec839f883dc1174d7f427
王启帆 <mrastaire@189.cn>
2020-02-15 14:08:42 +0800

    header完成99% 还差 ：点击搜索输入框，下拉搜索历史与推荐

--- js/index/index.js
+++ js/index/index.js
@@ -21,4 +21,51 @@ $(document).ready(function () {
 		$('.top_nav .location').removeClass('selected');
 		$('.top_nav .city_select').css('display', 'none');
 	});
+	/* 登录 鼠标移入登录 */
+	$('.top_nav .login').on('mouseenter', function () {
+		$('.top_nav .login .title').addClass('show');
+		$('.top_nav .login .login_box').css('display', 'inline-block')
+	});
+	$('.top_nav .login').on('mouseleave', function () {
+		$('.top_nav .login .title').removeClass('show');
+		$('.top_nav .login .login_box').css('display', 'none')
+	});
+	/* 我的特卖 会员俱乐部 客户服务 */
+	$('.top_nav .pull_down').on('mouseenter', function () {
+		$(this).find('.title').addClass('show');
+		$(this).find('.box').css('display', 'inline-block')
+	});
+	$('.top_nav .pull_down').on('mouseleave', function () {
+		$(this).find('.title').removeClass('show');
+		$(this).find('.box').css('display', 'none')
+	});
+	/* 移动端 二维码 */
+	$('.top_nav .mobileCode').on('mouseenter', function () {
+		$('.top_nav .mobileCode .title').addClass('show');
+		$('.top_nav .mobileCode .box').css('display', 'inline-block')
+	});
+	$('.top_nav .mobileCode').on('mouseleave', function () {
+		$('.top_nav .mobileCode .title').removeClass('show');
+		$('.top_nav .mobileCode .box').css('display', 'none')
+	});
+	/* 查看更多 */
+	$('.top_nav .more').on('mouseenter', function () {
+		$('.top_nav .more .title').addClass('show');
+		$('.top_nav .more .box').css('display', 'inline-block')
+	});
+	$('.top_nav .more').on('mouseleave', function () {
+		$('.top_nav .more .title').removeClass('show');
+		$('.top_nav .more .box').css('display', 'none')
+	});
+	/* 购物袋 */
+	$('.top_content .shoppingCart').on('mouseenter', function () {
+		$('.top_content .shoppingCart .title').addClass('show');
+		$('.top_content .shoppingCart').addClass('bgcolor');
+		$('.top_content .shoppingCart .box').css('display', 'inline-block');
+	});
+	$('.top_content .shoppingCart').on('mouseleave', function () {
+		$('.top_content .shoppingCart .title').removeClass('show');
+		$('.top_content .shoppingCart').removeClass('bgcolor');
+		$('.top_content .shoppingCart .box').css('display', 'none');
+	});
 });
