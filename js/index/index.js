010de60e37dd7b1453cd2168ffb22b775907bfe1
王启帆 <mrastaire@189.cn>
2020-02-17 23:08:21 +0800

    1、主页完成 95% 2、待完成：左右联动（反向） 3、倒计时

--- js/index/index.js
+++ js/index/index.js
@@ -1,129 +1,127 @@
 /* DOM加载完毕才能执行 */
 $(document).ready(function() {
-	/* 城市选择 */
-	$('.top_nav .location .show_city').on('click', function() {
-		//判断是否展开
-		var isShow = $('.top_nav .city_select').is(':visible');
-		if (!isShow) {
-			//展开选择
-			$('.top_nav .location').addClass('selected');
-			$('.top_nav .city_select').css('display', 'inline-block');
-			return;
-		} else {
-			//关闭选择
-			$('.top_nav .location').removeClass('selected');
-			$('.top_nav .city_select').css('display', 'none');
-			return;
-		}
+	/* -------------右侧固定栏------------ */
+	/* 用户中心 */
+	$('.scrollBar .user').on('mouseenter', function () {
+		$('.scrollBar .user_icon').addClass('active');
+		$('.scrollBar .user_box').stop(true).animate({'left':'-247px'});
 	});
-	$('.top_nav .location .del').on('click', function() {
-		//关闭选择
-		$('.top_nav .location').removeClass('selected');
-		$('.top_nav .city_select').css('display', 'none');
+	$('.scrollBar .user').on('mouseleave', function () {
+		$('.scrollBar .user_icon').removeClass('active');
+		$('.scrollBar .user_box').stop(true).animate({'left':'40px'});
 	});
-	/* 登录 鼠标移入登录 */
-	$('.top_nav .login').on('mouseenter', function() {
-		$('.top_nav .login .title').addClass('show');
-		$('.top_nav .login .login_box').css('display', 'inline-block')
+	/* 我的订单  我的唯品币 触摸 */
+	$('.scrollBar .user_box .link_item').eq(0).on('mouseenter', function () {
+		$(this).find('img').attr('src','img/index/order_%23df147f.png');
+		$(this).find('span').css('color','#df147f');
 	});
-	$('.top_nav .login').on('mouseleave', function() {
-		$('.top_nav .login .title').removeClass('show');
-		$('.top_nav .login .login_box').css('display', 'none')
+	$('.scrollBar .user_box .link_item').eq(0).on('mouseleave', function () {
+		$(this).find('img').attr('src','img/index/order_%23d9d9d9.png');
+		$(this).find('span').css('color','#666');
 	});
-	/* 我的特卖 会员俱乐部 客户服务 */
-	$('.top_nav .pull_down').on('mouseenter', function() {
-		$(this).find('.title').addClass('show');
-		$(this).find('.box').css('display', 'inline-block')
+	$('.scrollBar .user_box .link_item').eq(1).on('mouseenter', function () {
+		$(this).find('img').attr('src','img/index/rmb_%23df147f.png');
+		$(this).find('span').css('color','#df147f');
 	});
-	$('.top_nav .pull_down').on('mouseleave', function() {
-		$(this).find('.title').removeClass('show');
-		$(this).find('.box').css('display', 'none')
+	$('.scrollBar .user_box .link_item').eq(1).on('mouseleave', function () {
+		$(this).find('img').attr('src','img/index/rmb_%23d9d9d9.png');
+		$(this).find('span').css('color','#666');
 	});
-	/* 移动端 二维码 */
-	$('.top_nav .mobileCode').on('mouseenter', function() {
-		$('.top_nav .mobileCode .title').addClass('show');
-		$('.top_nav .mobileCode .box').css('display', 'inline-block')
-	});
-	$('.top_nav .mobileCode').on('mouseleave', function() {
-		$('.top_nav .mobileCode .title').removeClass('show');
-		$('.top_nav .mobileCode .box').css('display', 'none')
-	});
-	/* 查看更多 */
-	$('.top_nav .more').on('mouseenter', function() {
-		$('.top_nav .more .title').addClass('show');
-		$('.top_nav .more .box').css('display', 'inline-block')
-	});
-	$('.top_nav .more').on('mouseleave', function() {
-		$('.top_nav .more .title').removeClass('show');
-		$('.top_nav .more .box').css('display', 'none')
+	/* 关闭 */
+	$('.scrollBar .user_box b ').on('click', function () {
+		$('.scrollBar .user_icon').removeClass('active');
+		$('.scrollBar .user_box').stop(true).animate({'left':'40px'});
 	});
 	/* 购物袋 */
-	$('.top_content .shoppingCart').on('mouseenter', function() {
-		$('.top_content .shoppingCart .title').addClass('show');
-		$('.top_content .shoppingCart').addClass('bgcolor');
-		$('.top_content .shoppingCart .box').css('display', 'inline-block');
+	$('.scrollBar .shoppingCart .shoppingCart_icon').on('click', function () {
+		var isClick = $('.scrollBar .shoppingCart_box').hasClass('active');
+		if(!isClick){
+			$('.scrollBar .shoppingCart_box').stop(true).animate({'left':'-247px'});
+			$('.scrollBar .shoppingCart_box').addClass('active');
+		}else{
+			$('.scrollBar .shoppingCart_box').stop(true).animate({'left':'40px'});
+			$('.scrollBar .shoppingCart_box').removeClass('active');
+		}
 	});
-	$('.top_content .shoppingCart').on('mouseleave', function() {
-		$('.top_content .shoppingCart .title').removeClass('show');
-		$('.top_content .shoppingCart').removeClass('bgcolor');
-		$('.top_content .shoppingCart .box').css('display', 'none');
+	/* 关闭 */
+	$('.scrollBar .shoppingCart_box b ').on('click', function () {
+		$('.scrollBar .shoppingCart_box').stop(true).animate({'left':'40px'});
+		$('.scrollBar .shoppingCart_box').removeClass('active');
 	});
-	/* 搜索下拉框 */
-	$('.top_content .search input').on('focus', function() {
-		$(this).parents('.input_box').siblings('.pullDown').css('display', 'inline-block');
+	/* 其他菜单弹出 */
+	$('.scrollBar .unify_item').on('mouseenter', function () {
+		$(this).children('.unify_icon').addClass('active');
+		$(this).children('.unify_box').stop(true).animate({'left':'-122px'},200)
 	});
-	$('.top_content .search input').on('blur', function() {
-		$(this).parents('.input_box').siblings('.pullDown').css('display', 'none');
+	$('.scrollBar .unify_item').on('mouseleave', function () {
+		$(this).children('.unify_icon').removeClass('active');
+		$(this).children('.unify_box').stop(true).animate({'left':'0'},200)
 	});
-	/* 底部链接区 */
-	$('.footer .link ul li').on('mouseenter', function() {
-		$(this).css({
-			'color': 'rgb(250, 42, 131)',
-			'margin-left': '5px'
+	/* 返回顶部 */
+	$('.scrollBar .backTop .backTop_icon').click(function() {
+		$('html, body').animate({
+			scrollTop: 0
 		});
-		$(this).children('a').css('color', 'rgb(250, 42, 131)');
 	});
-	$('.footer .link ul li').on('mouseleave', function() {
-		$(this).css({
-			'color': 'grey',
-			'margin-left': '0px'
+	/* 唯品快抢  动态变化 */
+	function rotate(){
+		var isShow = $('.quickly .quickly_show .list_1').is(':visible');
+		if(isShow){
+			$('.quickly .quickly_show .list_1').hide("fast");
+			$('.quickly .quickly_show .list_2').show("fast");
+		}else{
+			$('.quickly .quickly_show .list_2').hide("fast");
+			$('.quickly .quickly_show .list_1').show("fast");
+		}
+	};
+	rotate();
+	var timer = setInterval(function () {
+		rotate();
+	},2000);
+	// /* --------------------倒计时---------------------*/
+	// /* 获取当前时间 */
+	// var nowTime = new Date();
+	// /* 设定目标时间 */
+	// var evenningTime = new Date().setHours(20);
+	// var morningTime = new Date().setHours(10);
+	// /* 判断距离时间段 */
+	// if(nowTime > 10 && nowTime < 20){
+	// 	/* 进行20:00倒计时 */
+		
+	// }else{
+	// 	/* 进行10:00倒计时 */
+		
+	// }
+	
+	/* 楼层跳转 */
+	$('.floor_nav li').click(function() {
+		//获取索引
+		var i = $(this).index();
+		//获取楼层的scrollTop
+		var top = $('.floor_index').eq(i).offset().top;
+		$('html, body').stop(true).animate({
+			scrollTop: top
 		});
-		$(this).children('a').css('color', 'grey');
-	});
-	/* 横向大导航栏 更多 */
-	$('.nav .crosswise_nav .more').on('mouseenter', function() {
+		//根据索引进行左右联动
+		$('.floor_nav li').removeClass('active');
 		$(this).addClass('active');
-		$(this).children('.more_box').css('display', 'inline-block');
 	});
-	$('.nav .crosswise_nav .more').on('mouseleave', function() {
-		$(this).removeClass('active');
-		$(this).children('.more_box').css('display', 'none');
-	});
-	/* 纵向大导航栏 移入下拉效果*/
-	$('.nav .vertical_nav').on('mouseenter', function() {
-		$(this).children('.channel_list').stop(true).slideDown();
-		$('.nav .vertical_nav .channel_list li').on('mouseenter', function() {
-			$('.nav .vertical_nav .channel_list li .list').hide();
-			$(this).children('.list').stop(true).show();
-		});
-	});
-	$('.nav .vertical_nav').on('mouseleave', function() {
-		$(this).children('.channel_list').stop(true).slideUp();
-		$('.nav .vertical_nav .channel_list li').on('mouseenter', function() {
-			$('.nav .vertical_nav .channel_list li .list').hide();
-			$(this).children('.list').stop(true).hide();
-		});
-	});
-	//scrollTop
-	var maxTop = $('.nav').offset().top;
+	//默认显示第一个
+	$('.floor_nav li').eq(0).addClass('active');
+	/* 楼层导航跟随 */
+	var floor_top = $('.update_title').offset().top;
 	$(window).scroll(function() {
 		//获取scrollTop == 滚动的距离
 		var scrollTop = $(this).scrollTop();
 		// //临界值
-		if (scrollTop > maxTop) {
-			$('.nav').addClass('fixed');
+		if (scrollTop > floor_top) {
+			$('.floor_nav').addClass('fixed');
 		} else {
-			$('.nav').removeClass('fixed');
+			$('.floor_nav li').removeClass('active');
+			$('.floor_nav li').eq(0).addClass('active');
+			$('.floor_nav').removeClass('fixed');
 		}
 	});
+	
 });
+
